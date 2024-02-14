9d08e35f5b303ac300dd17ae145cd5fc39574706
isidoridev <faircloughconor@gmail.com>
2024-02-14 01:44:08 +0000

    Setting Up

---
+++ README.md
@@ -0,0 +1,15 @@
+# Word Occurrence
+I want to see the context that words are used in.
+
+# Features
+- Parses text.
+- Stores text, word frequencies, and sentence occurrences.
+- Allows select word and number to display sentence. (WIP)
+
+## Maybe features
+- Sort most-to-least frequent (I will need to ban common words.)
+-  
+
+
+## Build 
+npx tsc && node build/wordOccurence.js
---
+++ src/wordOccurence.ts
@@ -0,0 +1,100 @@
+/* eslint-disable @typescript-eslint/brace-style */
+/* eslint-disable @typescript-eslint/naming-convention */
+const fs = require('fs')
+
+//  Words In Context
+//  Parsing Input To Blob
+
+// *Important word_set
+type WordSet = Record<string, number[]>
+let word_set: WordSet = {}
+
+// *Important sentence_list
+type SentenceList = string[]
+let sentence_list: SentenceList = []
+
+// Sorta Important
+interface TotalData {
+  sentence_list: SentenceList,
+  word_set: WordSet, 
+}
+
+let total_data: TotalData = {sentence_list: [], word_set: {}}
+function createTotalData(): TotalData {
+  return {word_set, sentence_list}
+}
+
+
+/// if(c == ' ') processWord(c,line);
+function processWord (word: string, line_count: number): void {
+  if (!(word in word_set)) {
+    word_set[word] = [line_count]
+  } else {
+    word_set[word].push(line_count)
+  }
+}
+
+function parseInputToFiles (input: string): void {
+  let prev_sentence: string = ''
+  let prev_word: string = ''
+  let sentence_count: number = 0
+  
+
+  for (let i = 0; i < input.length; i++) {
+    const c = input.charAt(i)
+
+    if (/^[a-zA-ZáéíóúÁÉÍÓÚüÜñÑ¿¡\-0-9]+$/.test(c)) {
+      // console.log(1, c)
+      prev_word += c
+    }
+    else if (c === ' ') { // End of word - collect last word and add to line.
+      // console.log(2, prev_word)
+      if (prev_word === '') continue
+      processWord(prev_word, sentence_count)
+      prev_sentence += `${prev_word} `
+      prev_word = ''
+    }
+    else if (c === '.' || c === '?' || c === '!') { // End of sentence - collect last word and sentence.
+      // console.log(3, c)
+      processWord(prev_word, sentence_count)
+      sentence_list[sentence_count] = `${prev_sentence} ${prev_word}${c}`
+      console.log(sentence_list)
+      prev_sentence = ''
+      prev_word = ''
+      sentence_count += 1
+    }
+  }
+  // console.log(sentence_list, sentence_count)
+}
+
+parseInputToFiles('This is a test piece. I wonder what is the output? Who knows.')
+total_data = createTotalData()
+
+// convert word_set into file
+function writeBlobToFile (path?: string) {
+  if (path) {
+    //
+  } else {
+    // Generic Path ~/Writing/12WordsInContext/.json
+    // console.log('Executing writeBlobToFile')
+    // Convert the array of objects to JSON string
+    const jsonString = JSON.stringify(total_data, null, 2)
+    fs.writeFileSync('./results/word_set.json', jsonString)
+    console.log('JSON file created successfully.')
+  }
+}
+
+writeBlobToFile()
+
+function restoreWordSet (): void {
+  // Read the JSON file
+  const jsonString = fs.readFileSync('./results/word_set.json', 'utf-8')
+  // Parse the JSON string into an array of MyObject
+  total_data = JSON.parse(jsonString)
+}
+
+console.log(total_data.sentence_list)
+
+//restoreWordSet()
+
+//console.log(word_set);
